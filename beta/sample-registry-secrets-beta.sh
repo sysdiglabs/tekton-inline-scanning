9eb79e12dca8052c442263d7260dbfb9a1625bc7
Vicente Herrera <vicente.herrera@sysdig.com>
2020-09-29 19:24:29 +0200

    Added working beta API pipeline example

---
+++ beta/sample-registry-secrets-beta.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+kubectl create secret docker-registry regcred \
+                    --docker-server=index.docker.io \
+                    --docker-username=<username> \
+                    --docker-password=<password> \
+                    --docker-email=<email>
