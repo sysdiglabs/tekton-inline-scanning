28f06cca151e88ee8ac06e607906ef539924a44c
Vicente Herrera <vicente.herrera@sysdig.com>
2020-09-30 17:58:07 +0200

    Minor fixes to sample Docker username usage, specifying namespace and removing unused environment variable

--- beta/sample-registry-secrets-beta.sh
+++ beta/sample-registry-secrets-beta.sh
@@ -4,4 +4,5 @@ kubectl create secret docker-registry regcred \
                     --docker-server=index.docker.io \
                     --docker-username=<username> \
                     --docker-password=<password> \
-                    --docker-email=<email>
+                    --docker-email=<email> \
+                     -n tekton-pipelines
